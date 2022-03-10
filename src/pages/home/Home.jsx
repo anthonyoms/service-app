d5313cdc4d8cf26db58755bd2d039eadbabc2bbf
Anthony <Anthony.otoniel@hotmail.com>
2022-03-09 21:33:09 -0400

    adding widgets

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
+import { data } from "../../dummyData";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
+import WidgetSm from "../../components/widgetSm/WidgetSm";
 
 export const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart data={data} title="User Analytics" grid dataKey="Active User" />
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
     </div>
   );
 };
