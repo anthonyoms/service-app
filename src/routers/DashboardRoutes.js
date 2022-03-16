380da603ef030b92ef45610c7407956c6025f18a
Anthony Otoniel Meléndez Suero <anthony.otoniel@hotmail.com>
2022-03-16 19:50:15 -0400

    routes done

--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { Sidebar } from "../components/sidebar/Sidebar";
 import Topbar from "../components/topbar/Topbar";
 import { Home } from "../pages/home/Home";
+import UserList from "../pages/userList/UserList";
 
 export const DashboardRoutes = () => {
   return (
@@ -12,7 +13,9 @@ export const DashboardRoutes = () => {
         <Sidebar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="*" element={<Navigate to="/home" />} />
+          <Route path="/users" element={<UserList />} />
+
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
     </>
