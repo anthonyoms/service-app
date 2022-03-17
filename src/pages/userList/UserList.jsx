06b941627cf404784212e635edf6fd79d7b969df
Anthony Melendez S <anmelendez@dgii.gov.do>
2022-03-16 20:53:58 -0400

    userList interface done

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -1,6 +1,78 @@
 import React from "react";
+import { DeleteOutline } from "@material-ui/icons";
+import { Link } from "react-router-dom";
+import { DataGrid } from "@mui/x-data-grid";
+import { useState } from "react";
+
+import { userRows } from "../../dummyData";
 import "./userList.css";
 
 export default function UserList() {
-  return <div className="userList">UserList</div>;
+  const [data, setData] = useState(userRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    { field: "username", headerName: "User name", width: 200 },
+    {
+      field: "user",
+      headerName: "User",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="userListUser">
+            <img className="userListImg" src={params.row.avatar} alt="" />
+            {params.row.username}
+          </div>
+        );
+      },
+    },
+    { field: "email", headerName: "Email", width: 200 },
+    {
+      field: "status",
+      headerName: "Status",
+      type: "number",
+      width: 120,
+    },
+    {
+      field: "transaction",
+      headerName: "Transction Volumen",
+      type: "number",
+      width: 160,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={"/user/" + params.row.id}>
+              <button className="userListEdit">Edit</button>
+            </Link>
+            <DeleteOutline
+              className="userListDelete"
+              onClick={() => handleDelete(params.row.id)}
+            />
+          </>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div className="userList">
+      {" "}
+      <DataGrid
+        rows={data}
+        columns={columns}
+        pageSize={8}
+        rowsPerPageOptions={[8]}
+        checkboxSelection
+      />
+    </div>
+  );
 }
